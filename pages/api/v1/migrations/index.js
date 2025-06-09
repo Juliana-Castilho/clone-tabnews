263c22e551ffa41d6eb0c8eec601a15f998f52b3
Juliana-Castilho <julie.castilho@gmail.com>
2025-06-08 22:13:16 -0300

    feat: add  endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,37 @@
+import database from "infra/database";
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigations.length > 0) {
+      return response.status(201).json(migratedMigations);
+    }
+
+    return response.status(200).json(migratedMigations);
+  }
+
+  return response.status(405).end();
+}
